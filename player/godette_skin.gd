d6f6f28427b1d9e95b7552e8f3fbf4c543d1a84b
c-dez <hdezcruzcarlos@gmail.com>
2025-04-12 23:25:13 -0600

    added change_face() para que cara parpadee

--- player/godette_skin.gd
+++ player/godette_skin.gd
@@ -5,12 +5,19 @@ extends Node3D
 @onready var second_attack_timer: Timer = get_node("SecondAttackTimer")
 @onready var set_attacking_false: Timer = get_node("SetAttackingFalse")
 @onready var extra_animation: AnimationNodeAnimation = get_node("AnimationTree").get_tree_root().get_node("ExtraAnimation")
+@onready var face_material: StandardMaterial3D = get_node("Rig/Skeleton3D/Godette_Head").get_surface_override_material(0)
+var rng := RandomNumberGenerator.new()
+
 var attacking: bool = false
 var squash_and_stretch: float = 1.0:
     set(value):
         squash_and_stretch = value
         var negative := 1.0 + (1.0 - squash_and_stretch)
-        scale = Vector3(negative,squash_and_stretch, negative)
+        scale = Vector3(negative, squash_and_stretch, negative)
+const faces = {
+    "default": Vector3.ZERO,
+    "blink": Vector3(0.0, 0.5, 0)
+}
 
 
 func _physics_process(_delta: float) -> void:
@@ -84,7 +91,18 @@ func hit() -> void:
     get_node("AnimationTree").set("parameters/AttackOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_ABORT)
     attacking = false
 
-func do_squash_and_stretch(value :float,duration:float = 0.1 ):
+
+func do_squash_and_stretch(value: float, duration: float = 0.1):
     var tween = create_tween()
-    tween.tween_property(self , "squash_and_stretch", value, duration) 
-    tween.tween_property(self , "squash_and_stretch", 1.0, duration *1.8). set_ease(Tween.EASE_IN_OUT) 
+    tween.tween_property(self, "squash_and_stretch", value, duration)
+    tween.tween_property(self, "squash_and_stretch", 1.0, duration * 1.8).set_ease(Tween.EASE_IN_OUT)
+
+func change_face(expression: String) -> void:
+    face_material.uv1_offset = faces[expression]
+    
+
+func _on_blink_timer_timeout() -> void:
+    change_face("blink")
+    await get_tree().create_timer(0.2).timeout
+    change_face("default")
+    get_node("BlinkTimer").wait_time = rng.randf_range(1.5, 4.5)
