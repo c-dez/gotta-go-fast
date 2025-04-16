6e47f9982021664222e5d23dbbde08ea17f51f0a
c-dez <hdezcruzcarlos@gmail.com>
2025-04-15 20:50:22 -0600

    _on_area_3d_body_entered()-> detectar cuando Boss se acerca a Player

--- entities/enemies/boss.gd
+++ entities/enemies/boss.gd
@@ -1,13 +1,17 @@
 extends Enemy
 
-### Se herada desde Enemy, asi puedo poner funcionalidades compartidas entre todos los enemigos sin repetir y override lo que sea necesario
+# Se herada desde Enemy, asi puedo poner funcionalidades compartidas entre todos los enemigos sin repetir y override lo que sea necesario
+## Nodo que contiene vatios Timers
 @onready var timers: Node = get_node("Timers")
+## boleano para dar seguimiento a cuando animacion "spining" se esta reproduciendo
+var is_spinning: bool = false
 
-### simple animations
+### Diccionario conteniendo referencias a animaciones
 const simple_attacks: Dictionary = {
 	"slice": "2H_Melee_Attack_Slice",
 	"spin": "2H_Melee_Attack_Spin",
 	"range": "1H_Melee_Attack_Stab",
+	"spining": "2H_Melee_Attack_Spinning",
 }
 
 func _physics_process(delta: float) -> void:
@@ -15,24 +19,35 @@ func _physics_process(delta: float) -> void:
 	pass
 
 
-### attack player
-
-
 func _on_attack_timer_timeout() -> void:
-	### cuando AttackTimer node llega a cero, se checa si jugador esta dentro de notice_radius
-		### si lo esta y  es a menos de 5.0 melee_attack_animation()
+	# cuando AttackTimer node llega a cero, se checa si jugador esta dentro de notice_radius
+		# si lo esta y  es a menos de 5.0 melee_attack_animation()
 	if is_player_in_notice_radius():
 		if position.distance_to(player.position) < 5.0:
 			melee_attack_animation()
-		### si no esta , range_attack_animation()
+		# si no esta , range_attack_animation()
 		else:
-			range_attack_animation()
+			# range_attack_animation()
+			spin_attack_animation()
 
-	# 4 animaciones que controlar
-	# 2 melee attacks
-	# 2 range attacks
+	
+func spin_attack_animation() -> void:
+	# cuando animation.play spining incrementar velocidad de movimmiento (tween)
+	## nuevo valor para Enemy.speed_modifier
+	var speed_modifier_spin := 4.0
+	var tween := create_tween()
+	tween.tween_property(self, "speed_modifier", speed_modifier_spin, 0.5)
 
-
+	tween.tween_method(_spin_transition, 0.0, 1.0, 0.3)
+	# Detiene el timer que controla cuando se realiza un ataque
+	timers.get_node("AttackTimer").stop()
+	is_spinning = true
+	
+## Transision a "spinning" animation en nodo SpinBlend en AnimationTree
+func _spin_transition(value: float) -> void:
+	get_node("AnimationTree").set("parameters/SpinBlend/blend_amount", value)
+	pass
+	
 func range_attack_animation() -> void:
 	stop_movement(1.5, 1.5)
 
@@ -45,3 +60,16 @@ func melee_attack_animation() -> void:
 	get_node("AnimationTree").set("parameters/AttackOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
 
 	pass
+
+func _on_area_3d_body_entered(_body: Node3D) -> void:
+	if is_spinning:
+		await get_tree().create_timer(rng.randf_range(1.0, 2.0)).timeout
+		var tween := create_tween()
+		## El mismo valor que default Enemy.speed_modifier
+		const speed_modifier_default := 1.0
+		# regresa speed_modifier a default = 1.0
+		tween.tween_property(self, "speed_modifier", speed_modifier_default, 3.0)
+		tween.tween_method(_spin_transition, 1.0, 0.0, 0.3)
+		is_spinning = false
+		# Reinicia timer que controla la cadencia en que ataques inician
+		timers.get_node("AttackTimer").start()
--- entities/enemies/enemy.gd
+++ entities/enemies/enemy.gd
@@ -1,43 +1,48 @@
+## Clase/Blueprint para enemigos
 class_name Enemy
-### Los enemigos heredan de esta escena/scrip
+
 extends CharacterBody3D
 
-### Referencia al Player usando grupos
+## Referencia a el primer nodo en grupo "Player"
 @onready var player: CharacterBody3D = get_tree().get_first_node_in_group("Player")
-### referencia a el mesh
+## referencia a el nodo skin que contiene Mesh3d
 @onready var skin: Node3D = get_node("Skin")
-
+## Velocidad de movimiento normal
 @export var walk_speed: float = 2.0
+## Distancia desde self para detectar a jugador
 @export var notice_radius: float = 30.0
+## Distancia desde self para melee attack jugador
 @export var attack_radius: float = 3.0
+## Modificador de velocidad, modificado para controlar distintas velocidades segun contexto
 var speed_modifier: float = 1.0
 
-## Animaciones
+## state_machine dentro de AnimationTree
 @onready var move_state_machine: AnimationNodeStateMachinePlayback = get_node("AnimationTree").get("parameters/MoveStateMachine/playback")
+## Animacion dentro de AnimationTree
 @onready var attack_animation: AnimationNodeAnimation = get_node("AnimationTree").get_tree_root().get_node("AttackAnimation")
-###rng
+## Objeto para distintos RNG
 var rng := RandomNumberGenerator.new()
 
-
+## Decide en base a la distancia de self con target(player) si moverse hacia el o no/ controla la velocidad de movimiento
 func move_to_player(_delta: float) -> void:
-	### position.distance_to() si la distancia es menor a notice_radius, entonces mueve hacia jugador
+	# position.distance_to() si la distancia es menor a notice_radius, entonces mueve hacia jugador
 	if is_player_in_notice_radius():
-		### Toma ala direccion de jugador(target) y moverse hacia el
+		## Toma ala direccion de jugador(target) y moverse hacia el
 		var target_direction: Vector3 = (player.position - position).normalized()
-		### Lo convierto a Vector2
+		# Lo convierto a Vector2
+		## target_vec2 = Vector2(target_direction.x, target_direction.z)
 		var target_vec2 := Vector2(target_direction.x, target_direction.z)
 		if position.distance_to(player.position) > attack_radius:
-		### si self esta a mas de attack_radius-> se mueve hacia player
-			### Lo paso a velocity e invoco move_and_slide() para mover se hacia esa direccion y multiplico por speed
+		# si self esta a mas de attack_radius-> se mueve hacia player
+			# Lo paso a velocity e invoco move_and_slide() para mover se hacia esa direccion y multiplico por speed
 			velocity = Vector3(target_vec2.x, 0, target_vec2.y) * walk_speed * speed_modifier
-			### animacion
+			# animacion
 			move_state_machine.travel("walk")
 		else:
 		# si no -> no se mueve
 			velocity = Vector3.ZERO
 			move_state_machine.travel("idle")
-		### Rotar a direccion de Player
-		### con este codigo consigo que el angulo sea correcto
+		## Angulo hacia que target(player) esta
 		var target_angle: float = - target_vec2.angle() + PI / 2
 		rotation.y = rotate_toward(rotation.y, target_angle, _delta * 6.0)
 	else:
@@ -46,15 +51,16 @@ func move_to_player(_delta: float) -> void:
 
 	move_and_slide()
 	pass
-
+## Funcion para detectar si Player esta dentro de notice_radius
 func is_player_in_notice_radius() -> bool:
-### funcion hecha por mi
+# funcion hecha por mi
 	return true if position.distance_to(player.position) < notice_radius else false
 	
-
+## Detener el movimiento
 func stop_movement(start_duration: float, end_duration: float) -> void:
-	### su intencion es modificar la velocidad de movimiento para acciones como atacar
-	var tween = create_tween()
+	const speed_modifier_default := 1.0
+	var tween := create_tween()
+	
 	tween.tween_property(self, "speed_modifier", 0.0, start_duration)
-	tween.tween_property(self, "speed_modifier", 1.0, end_duration)
+	tween.tween_property(self, "speed_modifier", speed_modifier_default, end_duration)
 	pass
